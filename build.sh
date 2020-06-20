00ffb84477d32cc96bea72f1efdc7564b6d08d95
Mark Leybourne <mleyb@users.noreply.github.com>
2020-06-20 14:21:55 +0100

    WIP

--- build.sh
+++ build.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-#install zip on debian OS, since microsoft/dotnet container doesn't have zip by default
-if [ -f /etc/debian_version ]
-then
-  apt -qq update
-  apt -qq -y install zip
-fi
-
-dotnet restore ./Service/Service.csproj
-dotnet lambda package --project-location Service --configuration release --framework netcoreapp2.1 --output-package Service/bin/release/netcoreapp2.1/deploy-package.zip
+make build
